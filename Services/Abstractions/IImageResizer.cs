9f851f146cd18648697dbb5ccf9df11f003366bc
BnnQ <1v25fa94@gmail.com>
2023-05-11 18:20:05 +0300

    Added IImageResizer service abstraction

--- Services/Abstractions/IImageResizer.cs
+++ Services/Abstractions/IImageResizer.cs
@@ -3,5 +3,7 @@ namespace ImageShrinkerWebJob.Services.Abstractions;
 public interface IImageResizer
 {
     public Task<Stream> ResizeImageAsync(string pathToImage, Size newSize);
+    public Task<Stream> ResizeImageAsync(Stream imageStream, string imageExtension, Size newSize);
     public Task ResizeImageAsync(string pathToImage, Size newSize, string pathToResult);
+    public Task ResizeImageAsync(Stream imageStream, Size newSize, string pathToResult);
 }
